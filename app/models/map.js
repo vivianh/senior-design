3812ee4ab7fbed1a1b2c1e0255b16c403e8b66d4
vivian <vivianhhuang@gmail.com>
2015-02-09 13:06:02 -0500

    mocked out config file to rendered map process

--- app/models/map.js
+++ app/models/map.js
@@ -4,8 +4,8 @@
   exports.Map = Backbone.Model.extend({
 
     defaults: {
-      width: 15,
-      height: 10,
+      width: exports.globals.MAP_WIDTH,
+      height: exports.globals.MAP_HEIGHT,
     },
 
     initialize: function (options) {
@@ -16,15 +16,37 @@
       var rows = _.range(this.get('height'));
       var cols = _.range(this.get('width'));
 
+      this.set('setup', this.mockSetupFile());
+
       return _.map(rows, function (row) {
         return _.map(cols, function (col) {
+          var className = '';
+
+          if (this.get('setup')[row]) {
+            var symbol = this.get('setup')[row].charAt(col);
+            if (symbol === exports.globals.CONFIG_SYMBOL_OBSTACLE) {
+              className = 'isObstacle';
+            } else if (symbol === exports.globals.CONFIG_SYMBOL_LOCK) {
+              className = 'isLock';
+            } else if (symbol === exports.globals.CONFIG_SYMBOL_KEY) {
+              className = 'isKey';
+            }
+          }
+
           return {
             row: row,
             col: col,
-            name: 'grass',
+            name: className,
           };
-        });
-      });
+        }, this);
+      }, this);
+    },
+
+    mockSetupFile: function () {
+      var file = 'xxxoooxxxoooooo ' +
+                 'KoooooooooooLoo ' +
+                 'xoooooxooooxxox ';
+      return file.split(' ');
     },
 
   });
---
+++ app/utils.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  exports.globals = {
+    MAP_WIDTH:              15,
+    MAP_HEIGHT:             10,
+    CONFIG_SYMBOL_OBSTACLE: 'x',
+    CONFIG_SYMBOL_LOCK:     'L',
+    CONFIG_SYMBOL_KEY:      'K',
+  };
+
+}());
