a05e473116eaccb5720f9f6dac52d1feee1a9764
Vivian Huang <vivianhhuang@gmail.com>
2015-04-28 18:46:14 -0400

    updating state appearance

--- app/views/state.js
+++ app/views/state.js
@@ -9,8 +9,18 @@
     },
 
     render: function (params) {
-      var compiledTemplate = this.template(this.model.toJSON());
+      var compiledTemplate = this.template(
+        _.extend(this.model.toJSON(), {
+          'timeElapsed': this.time(this.model.get('timeElapsed')),
+        })
+      );
       this.$el.html(compiledTemplate);
+    },
+
+    time: function (elapsedTime) {
+      var mins = Math.floor(elapsedTime / 60000);
+      var secs = ((elapsedTime % 60000) / 1000).toFixed(0);
+      return mins + ":" + (secs < 10 ? '0' : '') + secs;
     },
 
   });
